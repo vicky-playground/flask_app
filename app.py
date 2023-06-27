4a889aba9069b1a41415ebd99f0968140cd449af
vicky-playground <90204593+vicky-playground@users.noreply.github.com>
2023-06-26 21:20:13 -0400

    add CRUD operations

--- app.py
+++ app.py
@@ -1,18 +1,85 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, redirect, request, jsonify, render_template, url_for
 
-# create the flask app
 app = Flask(__name__)
 
-# home page
-@app.route('/')
-def home():
-    return render_template('home.html')
+# Sample data
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100, 'account': 'Checking'},
+    {'id': 2, 'date': '2023-06-02', 'amount': 200, 'account': 'Savings'},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300, 'account': 'Investment'}
+]
 
-# define the logic for reading the inputs from the home page 
-@app.route('/result', methods=['POST'])
-def result():
-    input = request.form.get('url')
-    return render_template('result.html', display=input)
+# Read operation: List all transactions
+@app.route("/")
+def get_transactions():
+    return render_template("transactions.html", transactions=transactions)
+
+# Create operation: Display add transaction form
+@app.route("/add", methods=["GET"])
+def add_transaction_form():
+    return render_template("form.html")
+
+# Create operation: Add new transaction
+@app.route("/add", methods=["POST"])
+def add_transaction():
+    date = request.form.get("date")
+    amount = float(request.form.get("amount"))
+    account = request.form.get("account")
+
+    # Generate a unique ID for the new transaction
+    new_id = len(transactions) + 1
+
+    # Create the new transaction object
+    new_transaction = {
+        'id': new_id,
+        'date': date,
+        'amount': amount,
+        'account': account
+    }
+
+    # Add the new transaction to the list
+    transactions.append(new_transaction)
+
+    # Redirect to the transactions list page
+    return redirect(url_for("get_transactions"))
+
+# Update operation: Display edit transaction form
+@app.route("/edit/<int:transaction_id>", methods=["GET"])
+def edit_transaction(transaction_id):
+    for transaction in transactions:
+        if transaction['id'] == transaction_id:
+            return render_template("edit.html", transaction=transaction)
+
+    # Transaction not found
+    return "Transaction not found"
+
+# Update operation: Update an existing transaction
+@app.route("/update/<int:transaction_id>", methods=["POST"])
+def update_transaction(transaction_id):
+    date = request.form.get("date")
+    amount = float(request.form.get("amount"))
+    account = request.form.get("account")
+
+    for transaction in transactions:
+        if transaction['id'] == transaction_id:
+            transaction['date'] = date
+            transaction['amount'] = amount
+            transaction['account'] = account
+            break
+
+    # Redirect to the transactions list page
+    return redirect(url_for("get_transactions"))
+
+# Delete operation: Delete a transaction
+@app.route("/delete/<int:transaction_id>")
+def delete_transaction(transaction_id):
+    for transaction in transactions:
+        if transaction['id'] == transaction_id:
+            transactions.remove(transaction)
+            break
+
+    # Redirect to the transactions list page
+    return redirect(url_for("get_transactions"))
 
 if __name__ == "__main__":
     app.run(debug=True)
